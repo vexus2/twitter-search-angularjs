0b2037ed01d213b3ba009a32c11cc136885db250
Vexus2 <rinrin900@gmail.com>
2012-10-16 08:37:15 +0900

    fixed bug.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 mainController = function($scope, $http){
     $scope.executeSearch = function(){
-      var url = 'http://search.twitter.com/search.json?q=';
+      var url = 'http://search.twitter.com/search.json?q='
         +$scope.query
         + '&callback=JSON_CALLBACK';
         $http.jsonp(url).success(function(data){
            $scope.results = data.results;
         });
-    };
-};
+    }
+}
